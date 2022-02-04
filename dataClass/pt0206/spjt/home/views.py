1d43d8d9fb20789bf4c8f17f1870192c4a5881e1
eDast95 <vawav@naver.com>
2022-02-04 18:11:08 +0900

    first commit

--- dataClass/pt0206/spjt/home/views.py
+++ dataClass/pt0206/spjt/home/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    context = {'user_id':'aaa','nickName':'길동스'}
+    context = {'user_id':'dfa','nickName':'파크동현'}
     #   count='2day' -> 변수 count전송시킴
     return render(request,'index.html',context)
